577cce0bf2e0f15b493db17635f2cc9a04f04ec0
Marcus Greiner <velotist@outlook.com>
2022-12-05 08:44:43 +0100

    Use default screen orientation

--- AwesomeApp.Android/MainActivity.cs
+++ AwesomeApp.Android/MainActivity.cs
@@ -5,7 +5,7 @@ using Android.OS;
 
 namespace AwesomeApp.Droid
 {
-    [Activity(Label = "AwesomeApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation | ConfigChanges.UiMode | ConfigChanges.ScreenLayout | ConfigChanges.SmallestScreenSize )]
+    [Activity(Label = "AwesomeApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, ScreenOrientation = ScreenOrientation.Portrait)]
     public class MainActivity : Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
