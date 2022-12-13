182cf6156514b8d4c10458b495356105dec17a31
Marcus Greiner <velotist@outlook.com>
2022-12-13 08:52:17 +0100

    Add succes item database

---
+++ AwesomeApp/Data/SuccessItemDatabase.cs
@@ -0,0 +1,59 @@
+﻿using AwesomeApp.Models;
+using SQLite;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace AwesomeApp.Data
+{
+    public class SuccessItemDatabase
+    {
+        static SQLiteAsyncConnection Database;
+
+        public static readonly AsyncLazy<SuccessItemDatabase> Instance = new AsyncLazy<SuccessItemDatabase>(async () =>
+        {
+            var instance = new SuccessItemDatabase();
+            CreateTableResult result = await Database.CreateTableAsync<SuccessItem>();
+
+            return instance;
+        });
+
+        public SuccessItemDatabase()
+        {
+            Database = new SQLiteAsyncConnection(Constants.DatabasePath, Constants.Flags);
+        }
+
+        public Task<List<SuccessItem>> GetItemsAsync()
+        {
+            return Database.Table<SuccessItem>().ToListAsync();
+        }
+
+        public Task<List<SuccessItem>> GetItemsNotDoneAsync()
+        {
+            return Database.QueryAsync<SuccessItem>("SELECT * FROM [SuccessItem]");
+        }
+
+        public Task<SuccessItem> GetItemAsync(int id)
+        {
+            return Database.Table<SuccessItem>()
+                .Where(i => i.ID == id)
+                .FirstOrDefaultAsync();
+        }
+
+        public Task<int> SaveItemAsync(SuccessItem item)
+        {
+            if (item.ID != 0)
+            {
+                return Database.UpdateAsync(item);
+            }
+            else
+            {
+                return Database.InsertAsync(item);
+            }
+        }
+
+        public Task<int> DeleteItemAsync(SuccessItem item)
+        {
+            return Database.DeleteAsync(item);
+        }
+    }
+}
