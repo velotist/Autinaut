6378c4442efc6fac930afed154b6a99e64a7cec3
Marcus Greiner <velotist@outlook.com>
2022-12-23 21:37:48 +0100

    Add resource

--- AwesomeApp.Android/Resources/Resource.designer.cs
+++ AwesomeApp.Android/Resources/Resource.designer.cs
@@ -16682,28 +16682,31 @@ namespace AwesomeApp.Droid
 			public const int notify_panel_notification_icon_bg = 2131165333;
 			
 			// aapt resource value: 0x7F070096
-			public const int preference_list_divider_material = 2131165334;
+			public const int plus = 2131165334;
 			
 			// aapt resource value: 0x7F070097
-			public const int sad = 2131165335;
+			public const int preference_list_divider_material = 2131165335;
 			
 			// aapt resource value: 0x7F070098
-			public const int shocked = 2131165336;
+			public const int sad = 2131165336;
 			
 			// aapt resource value: 0x7F070099
-			public const int spi = 2131165337;
+			public const int shocked = 2131165337;
 			
 			// aapt resource value: 0x7F07009A
-			public const int success = 2131165338;
+			public const int spi = 2131165338;
 			
 			// aapt resource value: 0x7F07009B
-			public const int test_custom_background = 2131165339;
+			public const int success = 2131165339;
 			
 			// aapt resource value: 0x7F07009C
-			public const int tooltip_frame_dark = 2131165340;
+			public const int test_custom_background = 2131165340;
 			
 			// aapt resource value: 0x7F07009D
-			public const int tooltip_frame_light = 2131165341;
+			public const int tooltip_frame_dark = 2131165341;
+			
+			// aapt resource value: 0x7F07009E
+			public const int tooltip_frame_light = 2131165342;
 			
 			static Drawable()
 			{
