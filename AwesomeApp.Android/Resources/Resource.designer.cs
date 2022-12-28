25a5ca22cf3498a3bfc5ec5efd5f3755b4c8c3d2
Marcus Greiner <velotist@outlook.com>
2022-12-28 20:33:18 +0100

    Update resources

--- AwesomeApp.Android/Resources/Resource.designer.cs
+++ AwesomeApp.Android/Resources/Resource.designer.cs
@@ -16496,73 +16496,73 @@ namespace AwesomeApp.Droid
 			public const int anger = 2131165271;
 			
 			// aapt resource value: 0x7F070058
-			public const int anxious = 2131165272;
+			public const int avd_hide_password = 2131165272;
 			
 			// aapt resource value: 0x7F070059
-			public const int avd_hide_password = 2131165273;
+			public const int avd_show_password = 2131165273;
 			
 			// aapt resource value: 0x7F07005A
-			public const int avd_show_password = 2131165274;
+			public const int black_spicon = 2131165274;
 			
 			// aapt resource value: 0x7F07005B
-			public const int black_spicon = 2131165275;
+			public const int btn_checkbox_checked_mtrl = 2131165275;
 			
 			// aapt resource value: 0x7F07005C
-			public const int btn_checkbox_checked_mtrl = 2131165276;
+			public const int btn_checkbox_checked_to_unchecked_mtrl_animation = 2131165276;
 			
 			// aapt resource value: 0x7F07005D
-			public const int btn_checkbox_checked_to_unchecked_mtrl_animation = 2131165277;
+			public const int btn_checkbox_unchecked_mtrl = 2131165277;
 			
 			// aapt resource value: 0x7F07005E
-			public const int btn_checkbox_unchecked_mtrl = 2131165278;
+			public const int btn_checkbox_unchecked_to_checked_mtrl_animation = 2131165278;
 			
 			// aapt resource value: 0x7F07005F
-			public const int btn_checkbox_unchecked_to_checked_mtrl_animation = 2131165279;
+			public const int btn_radio_off_mtrl = 2131165279;
 			
 			// aapt resource value: 0x7F070060
-			public const int btn_radio_off_mtrl = 2131165280;
+			public const int btn_radio_off_to_on_mtrl_animation = 2131165280;
 			
 			// aapt resource value: 0x7F070061
-			public const int btn_radio_off_to_on_mtrl_animation = 2131165281;
+			public const int btn_radio_on_mtrl = 2131165281;
 			
 			// aapt resource value: 0x7F070062
-			public const int btn_radio_on_mtrl = 2131165282;
+			public const int btn_radio_on_to_off_mtrl_animation = 2131165282;
 			
 			// aapt resource value: 0x7F070063
-			public const int btn_radio_on_to_off_mtrl_animation = 2131165283;
+			public const int charticon = 2131165283;
 			
 			// aapt resource value: 0x7F070064
-			public const int charticon = 2131165284;
+			public const int chartsymbol = 2131165284;
 			
 			// aapt resource value: 0x7F070065
-			public const int chartsymbol = 2131165285;
+			public const int code = 2131165285;
 			
 			// aapt resource value: 0x7F070066
-			public const int code = 2131165286;
+			public const int contempt = 2131165286;
 			
 			// aapt resource value: 0x7F070067
-			public const int contempt = 2131165287;
+			public const int design_fab_background = 2131165287;
 			
 			// aapt resource value: 0x7F070068
-			public const int design_fab_background = 2131165288;
+			public const int design_ic_visibility = 2131165288;
 			
 			// aapt resource value: 0x7F070069
-			public const int design_ic_visibility = 2131165289;
+			public const int design_ic_visibility_off = 2131165289;
 			
 			// aapt resource value: 0x7F07006A
-			public const int design_ic_visibility_off = 2131165290;
+			public const int design_password_eye = 2131165290;
 			
 			// aapt resource value: 0x7F07006B
-			public const int design_password_eye = 2131165291;
+			public const int design_snackbar_background = 2131165291;
 			
 			// aapt resource value: 0x7F07006C
-			public const int design_snackbar_background = 2131165292;
+			public const int disgust = 2131165292;
 			
 			// aapt resource value: 0x7F07006D
-			public const int disgust = 2131165293;
+			public const int diversity = 2131165293;
 			
 			// aapt resource value: 0x7F07006E
-			public const int diversity = 2131165294;
+			public const int fear = 2131165294;
 			
 			// aapt resource value: 0x7F07006F
 			public const int ic_arrow_down_24dp = 2131165295;
@@ -16712,16 +16712,22 @@ namespace AwesomeApp.Droid
 			public const int surprise = 2131165343;
 			
 			// aapt resource value: 0x7F0700A0
-			public const int test_custom_background = 2131165344;
+			public const int swipe_left = 2131165344;
 			
 			// aapt resource value: 0x7F0700A1
-			public const int timeline = 2131165345;
+			public const int swipe_right = 2131165345;
 			
 			// aapt resource value: 0x7F0700A2
-			public const int tooltip_frame_dark = 2131165346;
+			public const int test_custom_background = 2131165346;
 			
 			// aapt resource value: 0x7F0700A3
-			public const int tooltip_frame_light = 2131165347;
+			public const int timeline = 2131165347;
+			
+			// aapt resource value: 0x7F0700A4
+			public const int tooltip_frame_dark = 2131165348;
+			
+			// aapt resource value: 0x7F0700A5
+			public const int tooltip_frame_light = 2131165349;
 			
 			static Drawable()
 			{
