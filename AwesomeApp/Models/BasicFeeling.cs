de829d281c46d601b69341c3ae653a966c4e297b
Marcus Greiner <velotist@outlook.com>
2022-12-28 20:31:26 +0100

    Add basic feelings

---
+++ AwesomeApp/Models/BasicFeeling.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AwesomeApp.Models
+{
+    public class BasicFeeling
+    {
+        public string Name { get; set; }
+        public string MoodIcon { get; set; }
+    }
+}
