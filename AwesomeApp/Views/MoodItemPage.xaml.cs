9b980830402f2b4e65052d661eb4cac2a4ab6bff
Marcus Greiner <velotist@outlook.com>
2023-01-02 20:41:54 +0100

    Correct mood bindings - Restyle mood pages

--- AwesomeApp/Views/MoodItemPage.xaml.cs
+++ AwesomeApp/Views/MoodItemPage.xaml.cs
@@ -1,59 +1,66 @@
 ﻿using AwesomeApp.Data;
 using AwesomeApp.Models;
 using System;
-using System.Collections.ObjectModel;
 using Xamarin.Forms;
 
 namespace AwesomeApp.Views
 {
     public partial class MoodItemPage : ContentPage
     {
-        public ObservableCollection<BasicFeeling> Feelings { get; set; }
         public MoodItemPage()
         {
             InitializeComponent();
+            CarouselMoodViewModel carouselMoodViewModel = new CarouselMoodViewModel();
+
             BindingContext = new MoodItem { Date = DateTime.Now.ToString() };
+            moodsCarouselView.BindingContext = carouselMoodViewModel;
+            moodsCarouselView.SelectionChanged += Carousel_SelectionChanged;
         }
 
         private void Carousel_SelectionChanged(object sender, Syncfusion.SfCarousel.XForms.SelectionChangedEventArgs e)
         {
-            OnSaveClicked(sender, e);
+            MoodItem moodItem = (MoodItem)BindingContext;
+            _ = e.SelectedIndex;
+            switch (e.SelectedIndex)
+            {
+                case 0:
+                    moodItem.MoodIcon = "anger.png";
+                    moodItem.Mood = "Angry";
+                    break;
+                case 1:
+                    moodItem.MoodIcon = "contempt.png";
+                    moodItem.Mood = "Contempt";
+                    break;
+                case 2:
+                    moodItem.MoodIcon = "disgust.png";
+                    moodItem.Mood = "Disgust";
+                    break;
+                case 3:
+                    moodItem.MoodIcon = "fear.png";
+                    moodItem.Mood = "Fear";
+                    break;
+                case 4:
+                    moodItem.MoodIcon = "joy.png";
+                    moodItem.Mood = "Joy";
+                    break;
+                case 5:
+                    moodItem.MoodIcon = "sadness.png";
+                    moodItem.Mood = "Sadness";
+                    break;
+                case 6:
+                    moodItem.MoodIcon = "surprise.png";
+                    moodItem.Mood = "Surprise";
+                    break;
+                default:
+                    moodItem.MoodIcon = "disgust.png";
+                    moodItem.Mood = "Disgust";
+                    break;
+            }
         }
 
         private async void OnSaveClicked(object sender, Syncfusion.SfCarousel.XForms.SelectionChangedEventArgs e)
         {
             MoodItem moodItem = (MoodItem)BindingContext;
-            switch (e.SelectedIndex)
-            {
-                case 1:
-                    moodItem.MoodIcon = "anger.png";
-                    moodItem.Mood = "Angry";
-                    break;
-                case 2:
-                    moodItem.MoodIcon = "contempt.png";
-                    moodItem.Mood = "Contempt";
-                    break;
-                case 3:
-                    moodItem.MoodIcon = "disgust.png";
-                    moodItem.Mood = "Disgust";
-                    break;
-                case 4:
-                    moodItem.MoodIcon = "fear.png";
-                    moodItem.Mood = "Fear";
-                    break;
-                case 5:
-                    moodItem.MoodIcon = "joy.png";
-                    moodItem.Mood = "Joy";
-                    break;
-                case 6:
-                    moodItem.MoodIcon = "sadness.png";
-                    moodItem.Mood = "Sadness";
-                    break;
-                case 7:
-                    moodItem.MoodIcon = "surprise.png";
-                    moodItem.Mood = "Surprise";
-                    break;
-            }
             MoodItemDatabase database = await MoodItemDatabase.Instance;
             _ = await database.SaveItemAsync(moodItem);
             _ = await Navigation.PopAsync();
