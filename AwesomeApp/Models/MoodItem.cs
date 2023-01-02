5560f6aac911777792c246f0ca1be9d2fe6d1eeb
Marcus Greiner <velotist@outlook.com>
2023-01-02 20:39:54 +0100

    Add situation mood property

--- AwesomeApp/Models/MoodItem.cs
+++ AwesomeApp/Models/MoodItem.cs
@@ -8,6 +8,7 @@ namespace AwesomeApp.Models
         [PrimaryKey, AutoIncrement]
         public int ID { get; set; }
         public string Mood { get; set; }
+        public string MoodSituation { get; set; }
         public string MoodIcon { get; set; }
         public string Date { get; set; } = DateTime.Now.ToString();
     }
