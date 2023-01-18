0e6f7f4b313efee7428dfbea701f0391c6257c88
Marcus Greiner <velotist@outlook.com>
2023-01-18 19:17:15 +0100

    Cleanup code

--- Autinaut/AppShell.xaml.cs
+++ Autinaut/AppShell.xaml.cs
@@ -1,21 +1,12 @@
-﻿using System.Windows.Input;
-using Xamarin.Forms;
+﻿using Xamarin.Forms;
 
 namespace Autinaut
 {
     public partial class AppShell : Shell
     {
-        public ICommand GoBack { get; }
         public AppShell()
         {
             InitializeComponent();
-            BindingContext = this;
-            GoBack = new Command(GoBackButton);
-        }
-
-        public async void GoBackButton()
-        {
-            await Navigation.PopToRootAsync();
         }
     }
 }
