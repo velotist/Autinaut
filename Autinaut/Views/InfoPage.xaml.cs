d1524fd8066848975e16427ba919c7fde8b3c6a3
Marcus Greiner <velotist@outlook.com>
2023-01-27 11:29:43 +0100

    Customize info page - Include feedback via mail

--- Autinaut/Views/InfoPage.xaml.cs
+++ Autinaut/Views/InfoPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
 using Xamarin.Essentials;
 using Xamarin.Forms;
@@ -8,6 +9,7 @@ namespace Autinaut.Views
     public partial class InfoPage : ContentPage
     {
         private readonly Uri SpiHome = new Uri("https://autinaut.net");
+        private readonly string cancelDisplayAlert = "OK";
         public InfoPage()
         {
             InitializeComponent();
@@ -16,6 +18,37 @@ namespace Autinaut.Views
         private async void GoToWebsite_Clicked(object sender, EventArgs e)
         {
             await OpenBrowser(SpiHome);
+        }
+
+        private async void OnFeedbackClicked(object sender, EventArgs e)
+        {
+            await SendEmail("Feedback On Autinaut", "", new List<string>
+            {
+                "marcusgreiner@protonmail.com"
+            });
+        }
+
+        private async Task SendEmail(string subject, string body, List<string> recipients)
+        {
+            try
+            {
+                EmailMessage message = new EmailMessage
+                {
+                    Subject = subject,
+                    Body = body,
+                    To = recipients,
+                };
+
+                await Email.ComposeAsync(message);
+            }
+            catch (FeatureNotSupportedException)
+            {
+                await DisplayAlert("Feature Not Supported", "Email is not supported on this device", cancelDisplayAlert);
+            }
+            catch (Exception ex)
+            {
+                await DisplayAlert("Exception", ex.ToString(), cancelDisplayAlert);
+            }
         }
 
         private async Task OpenBrowser(Uri uri)
