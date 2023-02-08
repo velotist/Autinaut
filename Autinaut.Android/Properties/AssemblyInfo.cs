b8f903c3b57547dd8dd47ad7e6875518cb1e2a64
Marcus Greiner <velotist@outlook.com>
2023-02-08 19:04:52 +0100

    Cleanup code

--- Autinaut.Android/Properties/AssemblyInfo.cs
+++ Autinaut.Android/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 using System.Reflection;
 using System.Runtime.InteropServices;
 
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
 [assembly: AssemblyTitle("Autinaut.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
@@ -14,17 +11,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
 [assembly: AssemblyVersion("1.4.0.0")]
 [assembly: AssemblyFileVersion("1.4.0.0")]
 
-// Add some common permissions, these can be removed if not needed
 #if DEBUG
 [assembly: Application(Debuggable = true)]
 #else
