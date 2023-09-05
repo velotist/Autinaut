ae2887d1f7d3b906f0218c1b85acec792270999b
Marcus Greiner <velotist@outlook.com>
2023-09-05 12:14:12 +0200

    Limit number of characters

--- Autinaut/ViewModels/EmotionItemViewModel.cs
+++ Autinaut/ViewModels/EmotionItemViewModel.cs
@@ -10,7 +10,14 @@ public class EmotionItemViewModel : IDatabaseItem
     [PrimaryKey] [AutoIncrement] public int Id { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
     public string EmotionName { get; set; } = AppResources.TextEmotionFear;
-    public string EmotionSituation { get; set; }
+    private string _emotionSituation;
+    public string EmotionSituation
+    {
+        get => _emotionSituation;
+        set =>
+            _emotionSituation = value is { Length: > 500 } ? value.Substring(0, 500) :
+                value;
+    }
     public string EmotionIcon { get; set; } = "fear.png";
     public int IconId { get; set; } = 3;
     public int PositiveAffectBalance { get; set; }
--- Autinaut/ViewModels/SuccessItemViewModel.cs
+++ Autinaut/ViewModels/SuccessItemViewModel.cs
@@ -8,5 +8,12 @@ public class SuccessItemViewModel : IDatabaseItem
 {
     [PrimaryKey] [AutoIncrement] public int Id { get; set; }
     public DateTime Date { get; set; } = DateTime.Now;
-    public string SuccessNote { get; set; }
+    private string _successNote;
+    public string SuccessNote
+    {
+        get => _successNote;
+        set =>
+            _successNote = value is { Length: > 700 } ? value.Substring(0, 700) :
+                value;
+    }
 }
